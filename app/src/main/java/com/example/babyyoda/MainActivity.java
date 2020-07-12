2b3845759b85acf8cd86f48dbe9a717aa87b618f
umang-sinha <umangsinha12@gmail.com>
2020-07-12 17:08:55 +0530

    made changes

--- app/src/main/java/com/example/babyyoda/MainActivity.java
+++ app/src/main/java/com/example/babyyoda/MainActivity.java
@@ -1,14 +1,18 @@
-package com.example.babyyoda;
+package com.example.baby_yoda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
